199eb81c4f0cef225eceb6d53af06ecb08c5e031
HappyHappyJoyJoy <joyumali@gmail.com>
2016-07-22 15:56:45 -0700

    Added UI buttons & timer Label, set up IBOutlets & IBActions for time label, pause, stop & reset.  Wrote code for adding up time. Need to write code for pause and reset.

--- Timer/ViewController.swift
+++ Timer/ViewController.swift
@@ -10,17 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    func result() {
-        print("1 second has passed.")
+    var timer = NSTimer()
+    
+    // Defining the time variable here makes it a globale variable. If time variable is in the increaseTimer below, it will be reset to 0 each time the function runs.
+    var time = 0
+    
+    @IBOutlet weak var timeLabel: UILabel!
+
+    // INCREASE TIMER FUNCTION. And set label one second more than it was before.
+    func icreaseTimer() {
+        time++ // second is added each time.
+        timeLabel.text = "\(time)"   // sets the text of timeLabel to the time as a string. Can also be written as timeLabel.text = String(time)
     }
+    
+    @IBAction func startButton(sender: AnyObject) {
+        // launch timer here but define timer outside any of the IBActions.
+        timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("increaseTimer"), userInfo: nil, repeats: true)
+    }
+    
+    @IBAction func pauseButton(sender: AnyObject) {
+    }
+    
+    @IBAction func resetButton(sender: AnyObject) {
+    }
+    
+    
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        var timer = NSTimer()
-
-        timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("result"), userInfo: nil, repeats: true)
         
+        /* place timer here so it loads from the start and create function before viewDidLoad()
+        var timer = NSTimer()
+        timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("result"), userInfo: nil, repeats: true)
+        */
     }
 
     override func didReceiveMemoryWarning() {
